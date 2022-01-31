23dbf74a46cf47884d748b1c7b587cfda668720a
daxiangjuji666 <98742981+daxiangjuji666@users.noreply.github.com>
2022-01-31 19:08:32 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,9 +441,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
+        if (cps <= 5) return '无名水平';
+        if (cps <= 8) return '¥水平';
+        if (cps <= 10)  return 'ts水平';
         if (cps <= 15) return '您';
         return '人？';
     }
